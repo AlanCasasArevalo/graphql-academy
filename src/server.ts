985f47f653ad6da782a6bfd796bacfd950ec02fc
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 10:23:22 +0200

    [FEATURE][SERVER] Se cambia el tipo de servidor que va a ser servido, se agrega el endpoint principal y simplemente desde ahi se realizaran todas las peticiones a la api

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,29 @@ import express from 'express'
 import cors from 'cors'
 import compression from 'compression'
 import { createServer } from 'http'
+import { ApolloServer } from 'apollo-server-express'
+import expressPlayGround from 'graphql-playground-middleware-express'
+import schema from './schema'
 
 const app = express()
 app.use('*', cors())
 app.use(compression())
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Correcto desde la academia'
-  })
+const server = new ApolloServer({
+  schema,
+  introspection: true
 })
+
+server.applyMiddleware({ app })
+
+app.get('/', expressPlayGround({
+  endpoint:'/graphql'
+}))
 
 const PORT = 5200
 const httpServer = createServer(app)
 httpServer.listen({
   port: PORT
 }, () => {
-  console.log(`Aplicacion corriendo en el puerto http://localhost:${PORT}`);
+  console.log(`Aplicacion corriendo en el puerto http://localhost:${PORT}/graphql`);
 })
