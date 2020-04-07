fef1e708fa372b1499632b6532be6a472976d89f
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 16:29:15 +0200

    [FEATURE][ADD COURSE] Se crea una nueva funcionalidad para que no se pueda crear un nuevo curso que se haya creado previamente.

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -4,20 +4,26 @@ import _ from 'lodash'
 const mutation: IResolvers = {
   Mutation:{
     newCourse(_: void, { course }): any {
-      const courseAdded = {
-        id: String(database.courses.length+1),
-        title: course.title,
-        description: course.description,
-        classes: course.classes,
-        time: course.time,
-        logo: course.logo,
-        level: course.level,
-        path: course.path,
-        teacher: course.teacher,
-        reviews: []
+      let courses = database.courses
+      if (courses.filter(courseIntoDataBase => (courseIntoDataBase.title === course.title && courseIntoDataBase.classes === course.classes)).length === 0) {
+        const courseAdded = {
+          id: String(courses.length+1),
+          title: course.title,
+          description: course.description,
+          classes: course.classes,
+          time: course.time,
+          logo: course.logo,
+          level: course.level,
+          path: course.path,
+          teacher: course.teacher,
+          reviews: []
+        }
+        courses.push(courseAdded)
+        return courses
+      } else {
+        courses = []
+        return courses
       }
-      database.courses.push(courseAdded)
-      return database.courses
     }
 }
 }
