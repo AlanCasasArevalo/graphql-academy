a2f3636a35d829e3fe1b78ee84320e4ff474364b
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 16:09:07 +0200

    [FEATURE][ADD COURSE] Se crea la funcion para poder crear un nuevo curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -2,7 +2,24 @@ import { IResolvers } from 'graphql-tools'
 import { database } from '../data/database.store'
 import _ from 'lodash'
 const mutation: IResolvers = {
-
+  Mutation:{
+    newCourse(_: void, { course }): any {
+      const courseAdded = {
+        id: String(database.courses.length+1),
+        title: course.title,
+        description: course.description,
+        classes: course.classes,
+        time: course.time,
+        logo: course.logo,
+        level: course.level,
+        path: course.path,
+        teacher: course.teacher,
+        reviews: []
+      }
+      database.courses.push(courseAdded)
+      return database.courses
+    }
+}
 }
 
 export default mutation;
