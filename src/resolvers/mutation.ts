7d2265f4843ca6fdbd05289ab4aca1249a79d344
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 15:24:40 +0200

    [FEATURE][CRUD MUTATION] Se crea el esqueleto de las operaciones CRUD que se realizaran en el curso.

---
+++ src/resolvers/mutation.ts
@@ -0,0 +1,8 @@
+import { IResolvers } from 'graphql-tools'
+import { database } from '../data/database.store'
+import _ from 'lodash'
+const mutation: IResolvers = {
+
+}
+
+export default mutation;
