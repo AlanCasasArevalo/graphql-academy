758a290637ba30976d365ad870abe48d6ecda4dd
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 13:31:39 +0200

    [FEATURE][COURSES] Se agrega el endpoint de seleccion de curso por id

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -22,6 +22,33 @@ const query: IResolvers = {
     courses(): any {
       return database.courses
     },
+    course(_: void, { id }): any {
+      const  result = database.courses.filter( student => student.id === id ) [0]
+      if (result && typeof result !== 'undefined') {
+        return result
+      } else {
+        const studentDefault = {
+          id: '-1',
+          name: 'No se encontro ningun estudiante con ese id',
+          email: '',
+          courses: []
+        }
+        return {
+          id: '-1',
+          title: 'No se encontro ningun estudiante con ese id',
+          description: '',
+          classes: -1,
+          time: -1,
+          logo: '',
+          level: {},
+          path: '',
+          teacher: '',
+          student: studentDefault,
+          reviews: {}
+        }
+      }
+    },
+
   }
 }
 
