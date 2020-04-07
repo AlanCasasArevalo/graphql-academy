d5d28300051d1036022b764375de47c7fe9caaad
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2020-04-07 13:20:21 +0200

    [FEATURE][STUDENTS] Se crea el endpoint para obtener el usuario por id

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -5,6 +5,19 @@ const query: IResolvers = {
   Query: {
     students(): any {
       return database.students
+    },
+    student(_: void, { id }): any {
+      const  result = database.students.filter( student => student.id === id ) [0]
+      if (result && typeof result !== 'undefined') {
+        return result
+      } else {
+        return {
+          id: '-1',
+          name: 'No se encontro ningun estudiante con ese id',
+          email: '',
+          courses: []
+        }
+      }
     }
   }
 }
